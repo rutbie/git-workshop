dc22d05a9c5669b01bcf725604d8c6301ff90c88
Ruta Bielyte <ruta.bielyte@nordsec.com>
2024-03-08 08:38:50 +0200

    chore: initial commit

---
+++ playwright.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: false,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [['html', { open: 'never' }]],
+  use: {
+    headless: false,
+    screenshot: 'only-on-failure',
+    trace: 'on-first-retry',
+  },
+});
