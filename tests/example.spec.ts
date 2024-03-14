0e867d108c5b6c9d68e420b7896a9f5e49fabaeb
akvilejarma <akvile.jarmalaviciute@nordsec.com>
2024-03-14 11:15:31 +0200

    Add new feature

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,11 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('new test', async ({ page }) => {
+  await page.goto('https://playwright.dev/admin');
+
+  await page.getByRole('link', { name: 'Login' }).click();
+
+  await expect(page.getByRole('heading', { name: 'This content is secret' })).toBeVisible();
+})
