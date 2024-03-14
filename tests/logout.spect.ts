d418ed628aae7c07194dd82c2988854bc3a6927c
akvilejarma <akvile.jarmalaviciute@nordsec.com>
2024-03-14 11:28:16 +0200

    add logout test

---
+++ tests/logout.spect.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('should logout', async ({ page }) => {
+  await page.goto('https://playwright.dev/logout');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Logout/);
+});
