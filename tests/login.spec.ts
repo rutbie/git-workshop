ce662a8ff4da2a67d5ba38e31a54211d5e1a6e89
Ruta Bielyte <ruta.bielyte@nordsec.com>
2024-03-15 15:02:32 +0200

    feat: first message

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,6 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test('should login', async ({ page }) => {
   await page.goto('https://playwright.dev/login');
+  // my commnet
+
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Login/);
