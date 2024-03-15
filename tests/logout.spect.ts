75d971690d67d2ec5d77948f7980c24d9739c714
akvilejarma <akvile.jarmalaviciute@nordsec.com>
2024-03-15 13:45:33 +0200

    feat: create conflict

--- tests/logout.spect.ts
+++ tests/logout.spect.ts
@@ -4,7 +4,7 @@ test('should logout', async ({ page }) => {
   await page.goto('https://playwright.dev/logout');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Logout/);
+  await expect(page).toHaveTitle(/LogoutXDD/);
   // add comment
   // add another comment
 });
