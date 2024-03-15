9abc4f218d0efcad02393266f1919c1b450ce5e3
Ruta Bielyte <ruta.bielyte@nordsec.com>
2024-03-15 13:42:13 +0200

    fix: commit to master

--- tests/logout.spect.ts
+++ tests/logout.spect.ts
@@ -4,7 +4,11 @@ test('should logout', async ({ page }) => {
   await page.goto('https://playwright.dev/logout');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Logout/);
+  await expect(page).toHaveTitle(/LogoutMainChange/);
   // add comment
   // add another comment
+});
+
+test('should not logout and login', async ({ page }) => {
+  // checking
 });
